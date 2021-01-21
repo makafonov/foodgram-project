d5b5c818e371c9e0717deec59a85e7d5af48a84d
Denis S <makafonov@gmail.com>
2021-01-21 15:35:02 +0500

    Added registration pages

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -7,9 +6,5 @@ from apps.users import forms
 
 class SignUp(CreateView):
     form_class = forms.UserRegistrationForm
-    # success_url = reverse_lazy('recipes:index')
+    success_url = reverse_lazy('recipes:index')
     template_name = 'signup.html'
-
-
-# class UserLogin(LoginView):
-#     form_class = forms.UserLogin
