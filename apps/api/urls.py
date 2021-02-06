5b52fefde4d935d23b16b86f0f4b8da96b97eef1
Denis S <makafonov@gmail.com>
2021-02-06 13:42:08 +0500

    Optimized urls for api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,15 @@
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from apps.api import views
 
 router = DefaultRouter()
-router.register('v1/favorites', views.FavoriteApiView)
-router.register('v1/subscriptions', views.FollowApiView)
-router.register('v1/purchases', views.PurchaseApiView)
-router.register('v1/ingredients', views.IgredientApiView)
+router.register('favorites', views.FavoriteApiView)
+router.register('subscriptions', views.FollowApiView)
+router.register('purchases', views.PurchaseApiView)
+router.register('ingredients', views.IgredientApiView)
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
